4c2260f7ccf14bfa72dd7e765adb47d72ff723d3
Scott Werner <scott.werner.vt@protonmail.com>
2021-08-24 10:12:01 -0400

    Install spaceship theme and zsh plugins.

--- setup.sh
+++ setup.sh
@@ -6,6 +6,9 @@ echo "Distro:" "$(uname)"
 echo "Installing packages..."
 
 # Common packages
+git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+
 git clone https://github.com/zsh-users/zsh-autosuggestions.git "$ZSH_CUSTOM/plugins/zsh-autosuggestions"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting"
 
