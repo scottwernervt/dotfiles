b86b2fa99407284b1c346fa57a6a103d52460068
Scott Werner <scott.werner.vt@protonmail.com>
2021-08-24 15:37:26 -0400

    Add extra packages to install on osx.

--- setup.sh
+++ setup.sh
@@ -5,16 +5,6 @@ echo "OS: ${OSTYPE}"
 echo "Distro:" "$(uname)"
 echo "Installing packages..."
 
-# Common packages
-git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
-ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
-
-git clone https://github.com/zsh-users/zsh-autosuggestions.git "$ZSH_CUSTOM/plugins/zsh-autosuggestions"
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting"
-
-curl https://pyenv.run | bash
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
-
 # Linux packages
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     yay -S gron
@@ -22,9 +12,21 @@ fi
 
 # OSX packages
 if [[ "$OSTYPE" == "darwin"* ]]; then
+    brew install bzip2 openssl readline sqlite3 xz zlib
     brew install gron
     brew install --cask keycastr
 fi
+
+# Common packages
+git clone https://github.com/zsh-users/zsh-autosuggestions.git "$ZSH_CUSTOM/plugins/zsh-autosuggestions"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting"
+
+git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+
+curl https://pyenv.run | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
+git clone https://github.com/pyenv/pyenv-virtualenv.git $(pyenv root)/plugins/pyenv-virtualenv
 
 echo "Activating $HOME/.zshrc"
 source "$HOME/.zshrc"
